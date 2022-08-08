309cc31d3ae8771410c82009e9b84dff1a205d81
Vadim Kindl <vadim.kindl@me.com>
2022-08-08 19:07:58 +0200

    Initial Commit

---
+++ silm-extract/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  silm-extract
+//
+//  Created by Vadim Kindl on 08.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
